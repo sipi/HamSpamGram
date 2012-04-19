26553c400cb9965a74705d141f764e87194cfd99
Clément SIPIETER <c.sipieter@gmail.com>
2012-04-19 21:03:44 +0200

    Update

--- src/text2arff.d
+++ src/text2arff.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.file;
 import std.ascii;
 import std.utf;
 import std.cstream;
@@ -79,8 +80,43 @@ string[] getWords(string text)
 
 }
 
-void main()
+void word_count(string[] word_list, ref uint[string] dictionnary)
 {
+  for(uint i = 0; i < word_list.length; ++i)
+  {
+    ++dictionnary[word_list[i].toLower().idup];
+  }
+
+}
+
+void main(string[] files)
+{
+  uint num_file = 0;
+  uint[string][] dictionnaries;
+  dictionnaries.length = files.length;
+  
+  foreach(string file; files)
+  {
+    //saute le nom du program
+    if(num_file == 0)
+    {
+      ++num_file;
+      continue;
+    }          
+    
+    string[] words = getWords(cast(string)read(file));
+
+    word_count(words, dictionnaries[num_file]);
+
+    writeln(dictionnaries[num_file].length);
+    foreach(word; dictionnaries[num_file].keys)
+      writeln(word," - ",dictionnaries[num_file][word]);
+    
+    ++num_file;
+
+  }
+  
+ 
   
 }
 
